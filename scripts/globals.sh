82df2bf3eb2443c376e3cf7ee25648e4a19033f6
jlburky <133066541+jlburky@users.noreply.github.com>
2024-06-14 15:18:48 -0400

    Update globals.sh with ansicolors

--- scripts/globals.sh
+++ scripts/globals.sh
@@ -9,3 +9,30 @@ top_dir=`dirname ${script_dir}`
 
 # Get a timestamp for replacing existing files
 timestamp=$(date +"%Y%m%d%H%M")
+
+# Provide ANSI colors
+# Usage example:
+# print_info "This is good info!"
+
+RED='\033[1;31m'
+GRN='\033[1;32m'
+YEL='\033[1;33m'
+CYN='\e[36m'
+NC='\033[0m'  # no color
+
+print_info() {
+    echo -e ${GRN}${1}${NC}
+}
+
+print_warning() {
+    echo -e ${YEL}${1}${NC}
+}
+
+print_error() {
+    echo -e ${RED}${1}${NC}
+}
+
+print_exec_command() {
+    echo -e "${CYN}+ ${1}${NC}"
+    ${1}
+}
